d62ce505c638d2f92d3c47ad06db90e18ca71fd2
benjamin4 <benjamin4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-02 16:23:50 +0000

    Tagging version 1.8
    git-svn-id: https://plugins.svn.wordpress.org/geoip-detect/tags/1.8@1036828 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- api.php
+++ api.php
@@ -27,8 +27,24 @@ function geoip_detect_get_info_from_ip($ip)
  */
 function geoip_detect_get_info_from_current_ip()
 {
+	$ip = geoip_detect_get_client_ip();
+	
 	// TODO: Use Proxy IP if available
-	return geoip_detect_get_info_from_ip(@$_SERVER['REMOTE_ADDR']);
+	return geoip_detect_get_info_from_ip($ip);
+}
+
+/**
+ * Get client IP (even if it is behind a reverse proxy)
+ */
+function geoip_detect_get_client_ip() {
+	if (get_option('geoip-detect-has_reverse_proxy', 0) && isset($_SERVER["HTTP_X_FORWARDED_FOR"]))
+	{
+		$ip = @$_SERVER["HTTP_X_FORWARDED_FOR"];
+	} else {
+		$ip = @$_SERVER['REMOTE_ADDR'];
+	}
+	
+	return $ip;
 }
 
 /**
@@ -55,16 +71,16 @@ function _geoip_detect_get_external_ip_adress_without_cache()
 {
 	$ipservices = array(
 		'http://ipv4.icanhazip.com',
-		'http://ifconfig.me/ip',
+//		'http://ifconfig.me/ip', // seems to be slow
 		'http://ipecho.net/plain',
 		'http://v4.ident.me',
 		'http://bot.whatismyipaddress.com',
-		'http://ipv4.ipogre.com',
 		'http://ip.appspot.com',
 	);
 	
 	// Randomizing to avoid querying the same service each time
 	shuffle($ipservices);
+	$ipservices = apply_filters('geiop_detect_ipservices', $ipservices);
 	$ipservices = array_slice($ipservices, 0, 3);
 	
 	foreach ($ipservices as $url)
@@ -72,10 +88,17 @@ function _geoip_detect_get_external_ip_adress_without_cache()
 		$ret = wp_remote_get($url, array('timeout' => 1));
 
 		if (is_wp_error($ret)) {
-			if (WP_DEBUG)
-				echo 'Curl error: ' . $ret;
+			if (WP_DEBUG || defined('WP_TESTS_TITLE')) {
+				trigger_error('_geoip_detect_get_external_ip_adress_without_cache(): Curl error (' . $url . '): ' . $ret->get_error_message(), E_USER_NOTICE);
+			}
+		} else if (isset($ret['response']['code']) && $ret['response']['code'] != 200) {
+			if (WP_DEBUG || defined('WP_TESTS_TITLE')) {
+				trigger_error('_geoip_detect_get_external_ip_adress_without_cache(): HTTP error (' . $url . '): Returned code ' . $ret['response']['code'], E_USER_NOTICE);
+			}			
 		} else if (isset($ret['body'])) {
-			return trim($ret['body']);
+			$ip = trim($ret['body']);
+			if (preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', $ip))
+				return $ip;
 		}
 	}
 	return '0.0.0.0';
--- views/plugin_page.php
+++ views/plugin_page.php
@@ -14,7 +14,7 @@ $date_format = get_option('date_format') . ' ' . get_option('time_format')
 		<?php printf(__('Last updated: %s', 'geoip-detect'), $last_update ? date_i18n($date_format, $last_update) : __('Never', 'geoip-detect')); ?>
 	</p>
 
-	<?php if (!defined('GEOIP_DETECT_AUTO_UPDATE_DEACTIVATED')) : ?>
+	<?php if (!defined('GEOIP_DETECT_AUTO_UPDATE_DEACTIVATED') || !GEOIP_DETECT_AUTO_UPDATE_DEACTIVATED) : ?>
 	<p>
 		<?php printf(__('Next update: %s', 'geoip-detect'), $next_cron_update ? date_i18n($date_format, $next_cron_update) : __('Never', 'geoip-detect')); ?><br />
 		<em><?php _e('(The file is updated automatically once a week.)', 'geoip-detect'); ?></em>
@@ -23,15 +23,15 @@ $date_format = get_option('date_format') . ' ' . get_option('time_format')
 	
 	<form method="post" action="#">
 		<input type="hidden" name="action" value="update" />
-		<input type="submit" value="<?php _e('Update now'); ?>" />
+		<input type="submit" class="button button-primary" value="<?php _e('Update now'); ?>" />
 	</form>
 	
 	<br/>
 	<h3>GeoIP Lookup</h3>
 	<form method="post" action="#">
 		<input type="hidden" name="action" value="lookup" />
-		<input type="text" name="ip" value="<?php echo isset($_REQUEST['ip']) ? esc_attr($_REQUEST['ip']) : esc_attr($_SERVER['REMOTE_ADDR']); ?>" />
-		<input type="submit" value="<?php _e('Lookup', 'geoip-detect'); ?>" />
+		<input type="text" name="ip" value="<?php echo isset($_REQUEST['ip']) ? esc_attr($_REQUEST['ip']) : esc_attr(geoip_detect_get_client_ip()); ?>" />
+		<input type="submit" class="button button-secondary" value="<?php _e('Lookup', 'geoip-detect'); ?>" />
 	</form>
 	<?php if ($ip_lookup_result !== false) :
 			if (is_object($ip_lookup_result)) : ?>
@@ -70,8 +70,13 @@ $date_format = get_option('date_format') . ' ' . get_option('time_format')
 		<p>
 			<input type="checkbox" name="options[set_css_country]" value="1" <?php if ($options['set_css_country']) { echo 'checked="checked"'; } ?>>&nbsp;<?php _e('Add a country-specific CSS class to the &lt;body&gt;-Tag.', 'geoip-detect'); ?><br />
 		</p>
+		<?php if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) : ?>
 		<p>
-			<input type="submit" value="<?php _e('Save', 'geoip-detect'); ?>" />
+			<input type="checkbox" name="options[has_reverse_proxy]" value="1" <?php if ($options['has_reverse_proxy']) { echo 'checked="checked"'; } ?>>&nbsp;<?php printf(__('The server is behind a reverse proxy (With Proxy: %s - Without Proxy: %s)', 'geoip-detect'), $_SERVER['HTTP_X_FORWARDED_FOR'], $_SERVER['REMOTE_ADDR']); ?><br />
+		</p>
+		<?php endif; ?>
+		<p>
+			<input type="submit" class="button button-primary" value="<?php _e('Save', 'geoip-detect'); ?>" />
 		</p>
 	</form>
 	<p>
