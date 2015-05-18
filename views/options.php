cb6afb2550459f8e83c7a9b4576e83fd05197a6d
benjamin4 <benjamin4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-18 14:15:22 +0000

    Release 2.4.2
    git-svn-id: https://plugins.svn.wordpress.org/geoip-detect/trunk@1162725 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/options.php
+++ views/options.php
@@ -49,10 +49,10 @@ $options = $currentSource->getParameterHTML();
 		<input type="hidden" name="action" value="options" />
 		<h3>General Options</h3>
 		<p>
-			<input type="checkbox" name="options[set_css_country]" value="1" <?php if ($options['set_css_country']) { echo 'checked="checked"'; } ?>>&nbsp;<?php _e('Add a country-specific CSS class to the &lt;body&gt;-Tag.', 'geoip-detect'); ?><br />
+			<input type="checkbox" name="options[set_css_country]" value="1" <?php if (!empty($options['set_css_country'])) { echo 'checked="checked"'; } ?>>&nbsp;<?php _e('Add a country-specific CSS class to the &lt;body&gt;-Tag.', 'geoip-detect'); ?><br />
 		</p>
 		<p>
-			<input type="checkbox" name="options[has_reverse_proxy]" value="1" <?php if ($options['has_reverse_proxy']) { echo 'checked="checked"'; } ?>>&nbsp;The server is behind a reverse proxy<em>
+			<input type="checkbox" name="options[has_reverse_proxy]" value="1" <?php if (!empty($options['has_reverse_proxy'])) { echo 'checked="checked"'; } ?>>&nbsp;The server is behind a reverse proxy<em>
 			<span class="detail-box">
 			<?php if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) : ?>
 			<?php printf(__('(With Proxy: %s - Without Proxy: %s)', 'geoip-detect'), $_SERVER['HTTP_X_FORWARDED_FOR'], $_SERVER['REMOTE_ADDR']); ?><br />
@@ -67,6 +67,14 @@ $options = $currentSource->getParameterHTML();
 			<input type="submit" class="button button-primary" value="<?php _e('Save', 'geoip-detect'); ?>" />
 		</p>
 	</form>
+	<?php if (!$ipv6_supported) : ?>
+	<div class="geoip_detect_error">
+		<h4>IPv6 not supported</h4>
+		<p>
+			Your version of PHP is compiled without IPv6-support, so it is not possible to lookup adresses like "2001:4860:4801:5::91". For more information see <a href="https://php.net/manual/en/function.inet-pton.php">PHP documentation & user comments</a>.
+		</p>
+	</div>
+	<?php endif; ?>
 	<p>
 		<br />
 		<small><em>This product includes GeoLite2 data created by MaxMind, available from <a href="http://www.maxmind.com/">http://www.maxmind.com</a>.</em></small>
